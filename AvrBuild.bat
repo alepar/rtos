b5bf847edb9ee2baea9393ab899d0c5c5b197493
alepar <alepar@gmail.com>
2010-03-27 15:43:54 +0300

    played with project files (renaming mostly)

--- AvrBuild.bat
+++ AvrBuild.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-"D:\avr\studio4\AvrAssembler2\avrasm2.exe" -S "D:\avr\work\rtos_atmega168\labels.tmp" -fI -W+ie -C V2E -o "D:\avr\work\rtos_atmega168\Demo16-RTOS.hex" -d "D:\avr\work\rtos_atmega168\Demo16-RTOS.obj" -e "D:\avr\work\rtos_atmega168\Demo16-RTOS.eep" -m "D:\avr\work\rtos_atmega168\Demo16-RTOS.map" "D:\avr\work\rtos_atmega168\Demo16-RTOS.asm"
+"D:\avr\studio4\AvrAssembler2\avrasm2.exe" -S "D:\avr\work\rtos_atmega168\labels.tmp" -fI -W+ie -C V2E -o "D:\avr\work\rtos_atmega168\rtos.hex" -d "D:\avr\work\rtos_atmega168\rtos.obj" -e "D:\avr\work\rtos_atmega168\rtos.eep" -m "D:\avr\work\rtos_atmega168\rtos.map" "D:\avr\work\rtos_atmega168\main.asm"
